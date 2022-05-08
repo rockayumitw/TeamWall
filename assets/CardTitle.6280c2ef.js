f315f84bfd9a28d543c7f90c1fe6a0a0008c1550
stark920 <hautang920@gmail.com>
2022-05-08 14:38:43 +0800

    deploy

---
+++ assets/CardTitle.6280c2ef.js
@@ -0,0 +1,1 @@
+import{o as t,c as r,t as o}from"./index.37ab5542.js";const s={class:"shadow-block relative mb-8 flex items-center justify-center border-2 border-black bg-white p-5 font-azeret text-xl"},l={props:{title:{type:String}},setup(e){return(a,c)=>(t(),r("div",s,o(e.title),1))}};export{l as _};
